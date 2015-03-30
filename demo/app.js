057dc867b3df5fccde7d03b806758af6133a98ec
jedanput <alajmovic.samir@gmail.com>
2015-03-30 14:08:54 +0200

    initial

--- demo/app.js
+++ demo/app.js
@@ -1,29 +1,20 @@
 (function () {
     'use strict';
 
-    var app = angular.module('app', ['ngTooltip', 'ngAnimate'])
+    angular.module('app', ['ngTooltip', 'ngAnimate'])
     .run(function($templateCache, $http){
-    });
+    })
+    .controller('mainCtrl', function($scope){
 
-    app.controller('mainCtrl', function($scope){
-
-            $scope.tpl1 = '<div class="tooltip tooltip-1 margin-bottom anim">'+
+            $scope.tpl1 = '<div class="tooltip margin-bottom anim">'+
                             'Tooltip<div class="arrow-down"></div>'+
                          '</div>';
 
-            $scope.tpl2 = '<div class="tooltip anim">'+
-                            'Tooltip<div class=""></div>'+
+            $scope.tpl2 = '<div class="tooltip-2 margin-left">'+
+                            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.<div class=""></div>'+
                          '</div>';
 
-            $scope.tpl3 = '<div class="tooltip tooltip-4">'+
-                            'Tooltip'+
-                         '</div>';
-
-            $scope.tpl4 = '<div class="tooltip border margin-right anim">'+
-                            'Tooltip<div class="arrow-right"></div>'+
-                         '</div>';
-
-            $scope.tpl5 = '<div class="tooltip">'+
+            $scope.tpl3 = '<div class="tooltip anim">'+
                             'Tooltip'+
                          '</div>';
     });
